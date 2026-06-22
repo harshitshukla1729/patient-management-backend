35d1058715bca690efd125a63f64be24a63ad413
Harshit Shukla <harshitshukla1290@gmail.com>
2026-06-22 11:55:11 +0530

    added integration tests using rest-assured to test api endpoints

---
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PatientIntegrationTest {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnPatientsWithValidToken() {
+        String loginPayload = """
+                    {
+                        "email": "testuser@test.com",
+                        "password": "password123"
+                    }
+                """;
+
+        String token = RestAssured.given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("token");
+
+        RestAssured.given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/patients")
+                .then()
+                .statusCode(200)
+                .body("patients", notNullValue());
+    }
+}
